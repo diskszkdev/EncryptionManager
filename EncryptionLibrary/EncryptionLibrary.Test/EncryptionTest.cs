d5c76325c6a848be0e4e831b633dbb5b8e8728fd
diskszkdev <diskszkdev@gmail.com>
2019-11-13 18:20:58 +0900

    create new project

---
+++ EncryptionLibrary/EncryptionLibrary.Test/EncryptionTest.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EncryptionLibrary.Test
+{
+    /// <summary>
+    /// 暗号化・復号機能のテストクラス
+    /// </summary>
+    [TestClass]
+    public class EncryptionTest
+    {
+        private Encryption _encryption;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _encryption = new Encryption();
+        }
+
+        /// <summary>
+        /// Encryptメソッドの引数がnullの場合、戻り値はnullとなる
+        /// </summary>
+        [TestMethod]
+        public void Encrypt_TextIsNull()
+        {
+            string text = null;
+            var result = _encryption.Encrypt(text);
+
+            Assert.AreEqual(null, result);
+        }
+
+        /// <summary>
+        /// Encryptメソッドの引数が空の場合、戻り値はnullとなる
+        /// </summary>
+        [TestMethod]
+        public void Encrypt_TextIsEmpty()
+        {
+            string text = "";
+            var result = _encryption.Encrypt(text);
+
+            Assert.AreEqual(null, result);
+        }
+
+        /// <summary>
+        /// Encryptメソッドの引数が空白の場合、戻り値はnullとなる
+        /// </summary>
+        [TestMethod]
+        public void Encrypt_TextIsWhiteSpace()
+        {
+            string text = " ";
+            var result = _encryption.Encrypt(text);
+
+            Assert.AreEqual(null, result);
+        }
+
+        /// <summary>
+        /// 暗号化に成功していること
+        /// </summary>
+        [TestMethod]
+        public void Encrypt_Success()
+        {
+            string text = "test";
+            var result = _encryption.Encrypt(text);
+
+            Assert.AreNotEqual(text, result);
+        }
+
+        /// <summary>
+        /// Decryptメソッドの引数がnullの場合、戻り値はnullとなる
+        /// </summary>
+        [TestMethod]
+        public void Decrypt_TextIsNull()
+        {
+            string base64Text = null;
+            var result = _encryption.Decrypt(base64Text);
+
+            Assert.AreEqual(null, result);
+        }
+
+        /// <summary>
+        /// Decryptメソッドの引数が空の場合、戻り値はnullとなる
+        /// </summary>
+        [TestMethod]
+        public void Decrypt_TextIsEmpty()
+        {
+            string base64Text = "";
+            var result = _encryption.Decrypt(base64Text);
+
+            Assert.AreEqual(null, result);
+        }
+
+        /// <summary>
+        /// Decryptメソッドの引数が空白の場合、戻り値はnullとなる
+        /// </summary>
+        [TestMethod]
+        public void Decrypt_TextIsWhiteSpace()
+        {
+            string base64Text = " ";
+            var result = _encryption.Decrypt(base64Text);
+
+            Assert.AreEqual(null, result);
+        }
+
+        /// <summary>
+        /// 復号に成功していること
+        /// </summary>
+        [TestMethod]
+        public void Decrypt_Success()
+        {
+            string base64Text = _encryption.Encrypt("test");
+            var result = _encryption.Decrypt(base64Text);
+
+            Assert.AreEqual("test", result);
+        }
+
+        /// <summary>
+        /// Decryptメソッドの引数が正しく暗号化された文字列でない場合、復号に失敗すること
+        /// </summary>
+        [TestMethod]
+        public void Decrypt_Fail()
+        {
+            string text = "test";
+            string errorMessage = "復号化するデータの長さが無効です。";
+
+            var ex = Assert.ThrowsException<CryptographicException>(() => _encryption.Decrypt(text));
+            Assert.AreEqual(errorMessage, ex.Message);
+        }
+    }
+}
---
+++ EncryptionLibrary/EncryptionLibrary/Encryption.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EncryptionLibrary
+{
+    /// <summary>
+    /// 文字列の暗号化と復号を管理する
+    /// </summary>
+    public class Encryption
+    {
+        #region Private consts
+
+        /// <summary>
+        /// 初期化ベクトル
+        /// </summary>
+        /// <remarks>
+        /// 16文字(8bit*16文字=128bit)
+        /// </remarks>
+        private const string AES_IV = "EncryptionAES_IV";
+
+        /// <summary>
+        /// 暗号化鍵
+        /// </summary>
+        /// <remarks>
+        /// 16文字(8bit*16文字=128bit)
+        /// </remarks>
+        private const string AES_KEY = "EncryptionAESKEY";
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// 文字列をAES暗号化する
+        /// </summary>
+        /// <param name="text">暗号化する文字列</param>
+        /// <returns>暗号化した文字列(Base64形式)</returns>
+        public string Encrypt(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) return null;
+
+            // 暗号化鍵と初期ベクトルをバイト配列に変換
+            byte[] key = Encoding.UTF8.GetBytes(AES_KEY);
+            byte[] iv = Encoding.UTF8.GetBytes(AES_IV);
+
+            // 暗号化対象の文字列をバイト配列に変換
+            byte[] src = Encoding.UTF8.GetBytes(text);
+
+            /* 
+            以下の設定はデフォルト値となっているので特別な設定は行わない。
+            ブロックサイズ:128bit
+            暗号化利用モード:CBC
+            パディング:PKCS7
+            */
+            using (var am = new AesManaged())
+            using (var encryptor = am.CreateEncryptor(key, iv))
+            using (var outStream = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(outStream, encryptor, CryptoStreamMode.Write))
+                {
+                    cs.Write(src, 0, src.Length);
+                }
+
+                byte[] result = outStream.ToArray();
+                return Convert.ToBase64String(result);
+            }
+        }
+
+        /// <summary>
+        /// Base64形式の文字列をAES復号する
+        /// </summary>
+        /// <param name="base64Text">暗号化された文字列(Base64形式)</param>
+        /// <returns>復号した文字列</returns>
+        public string Decrypt(string base64Text)
+        {
+            if (string.IsNullOrWhiteSpace(base64Text)) return null;
+
+            // 暗号化鍵と初期ベクトルをバイト配列に変換
+            byte[] key = Encoding.UTF8.GetBytes(AES_KEY);
+            byte[] iv = Encoding.UTF8.GetBytes(AES_IV);
+
+            // 復号対象の文字列をバイト配列に変換
+            byte[] src = Convert.FromBase64String(base64Text);
+
+            /* 
+            以下の設定はデフォルト値となっているので特別な設定は行わない。
+            ブロックサイズ:128bit
+            暗号化利用モード:CBC
+            パディング:PKCS7
+            */
+            using (var am = new AesManaged())
+            using (var decryptor = am.CreateDecryptor(key, iv))
+            using (var instream = new MemoryStream(src, false))
+            using (var outStream = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(instream, decryptor, CryptoStreamMode.Read))
+                {
+                    // バッファーサイズはBlockSizeの倍数用意する
+                    int i = 4096;
+                    byte[] buffer = new byte[i];
+                    int len = 0;
+                    while ((len = cs.Read(buffer, 0, i)) > 0)
+                    {
+                        outStream.Write(buffer, 0, len);
+                    }
+                }
+                byte[] result = outStream.ToArray();
+                return Encoding.UTF8.GetString(result);
+            }
+        }
+        #endregion
+    }
+}
